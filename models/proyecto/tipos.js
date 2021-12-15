4fe1d09f84a2ebae8df62f108763764c1c2d8cbc
JuanA <juanandreslujan30@gmail.com>
2021-12-15 10:31:35 -0500

    finalizar crud de objetivos

--- models/proyecto/tipos.js
+++ models/proyecto/tipos.js
@@ -14,6 +14,21 @@ const tiposProyecto = gql`
         tipo:Enum_TipoObjetivo!
     } 
 
+    input camposObjetivo{
+        descripcion: String!,
+        tipo: Enum_TipoObjetivo!
+    }
+
+    input camposProyecto{
+        nombre: String
+        presupuesto: Float
+        fechaInicio: Date
+        fechaFinal: Date
+        estado: Enum_EstadoProyecto
+        fase: Enum_FaseProyecto
+        # lider: 
+    }
+
 
     type Proyecto {
         _id: ID!
@@ -43,6 +58,15 @@ const tiposProyecto = gql`
         # lider:
         objetivos:[crearObjetivo]
         ): Proyecto
+
+        editarProyecto(id: String!,campos: camposProyecto!): Proyecto
+
+        crearObjetivo(idProyecto: String!,campos: camposObjetivo!):Proyecto
+
+        editarObjetivo(idProyecto:String!, indexObjetivo:Int!, campos:camposObjetivo!):Proyecto
+
+        eliminarObjetivo(idProyecto: String!, idObjetivo: String!): Proyecto    
+
     }
 `
 
