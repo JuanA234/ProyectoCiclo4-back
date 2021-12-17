b296eb15330a627c0d07d885fe18e6a9e379d797
Esteban <estebin5372@gmail.com>
2021-12-17 16:42:16 -0500

    Borrando lineas de código innecesarias

--- graphql/types.js
+++ graphql/types.js
@@ -20,16 +20,3 @@ export const tipos = [
   tiposAutenticacion,
 ];
 
-import {gql} from 'apollo-server-express';
-import { tiposAvance } from '../models/avance/tipos.js';
-import { tiposEnums } from '../models/enums/tipos.js';
-import { tiposProyecto } from '../models/proyecto/tipos.js';
-import { tiposUsuario } from '../models/usuario/tipos.js';
-
-const tiposGlobales = gql`
-    scalar Date
-`
-
-
-export const tipos = [tiposGlobales, tiposProyecto, tiposAvance, tiposEnums, tiposUsuario];
-
